b752d4b1e1f488e603a6dac4a4a2b01937188939
soyab-mostofa <soyabmostofa@gmail.com>
2024-06-06 13:05:11 +0600

    Added kinde auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,15 @@ import React from "react";
 import NavbarLinks from "./NavbarLinks";
 import { Button } from "./ui/button";
 import MobileMenu from "./MobileMenu";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <div className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
       <div className="md:col-span-3">
@@ -14,8 +21,19 @@ const Navbar = () => {
       </div>
       <NavbarLinks />
       <div className="flex items-center ms-auto gap-x-2 md:col-span-3">
-        <Button> Get Started</Button>
-        <Button variant="outline">Sign in</Button>
+        {user ? (
+          <div>user is authenticated</div>
+        ) : (
+          <div className="flex items-center gap-x-2">
+            <Button asChild>
+              <RegisterLink>Get Started</RegisterLink>
+            </Button>
+            <Button variant="outline" asChild>
+              <LoginLink>Sign in</LoginLink>
+            </Button>
+          </div>
+        )}
+
         <div className="md:hidden">
           <MobileMenu />
         </div>
